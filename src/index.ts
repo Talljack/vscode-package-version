c7f5dd809ce8c6c440b2f66fd210cc9fb2703402
Yugang Cao <1442770751@qq.com>
2024-02-22 22:55:51 +0800

    feat: done

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,84 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as path from 'path';
-import { parsePackageJson, getLatestVersion } from './utils';
+import * as vscode from "vscode";
+import * as path from "path";
+import { exec as execCallback } from "child_process";
+import {
+  parsePackageJson,
+  compareVersions,
+  packageVersionColorMap,
+  getLatestVersion,
+} from "./utils";
+
+import { promisify } from "util";
+
+// 将 exec 转换为返回 Promise 的版本
+const exec = promisify(execCallback);
+
+async function getPackageVersion(packageName: string): Promise<string> {
+  try {
+    const { stdout } = await exec(`npm view ${packageName} version`);
+    return stdout.trim(); // 使用 trim() 来去除可能的换行符
+  } catch (error) {
+    // 错误处理
+    console.error(`Error fetching package version: ${error}`);
+    throw error; // 或者返回一个默认值/错误值
+  }
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	console.log('start start activate');
-	context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(async (document) => {
-		console.log('00000', document.uri.fsPath);
-			if (path.basename(document.uri.fsPath) === 'package.json') {
-					const packageJson = parsePackageJson(document.uri);
-					if (!packageJson) {return;}
-					console.log('11111', packageJson);
-					const editor = vscode.window.activeTextEditor;
-					if (editor) {
-							const decorationsArray: vscode.DecorationOptions[] = [];
-							const decorationType = vscode.window.createTextEditorDecorationType({ after: { margin: '0 0 0 3em' } });
+  context.subscriptions.push(
+    vscode.workspace.onDidOpenTextDocument(async (document) => {
+      if (path.basename(document.uri.fsPath).includes("package.json")) {
+        const packageJson = parsePackageJson(document.uri);
+        if (!packageJson) {
+          return;
+        }
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+          const decorationsArray: vscode.DecorationOptions[] = [];
+          const decorationType = vscode.window.createTextEditorDecorationType({
+            after: { margin: "0 0 0 3em" },
+          });
 
-							const dependencies = packageJson.dependencies ?? {};
-							const devDependencies = packageJson.devDependencies ?? {};
-							const allDependencies = { ...dependencies, ...devDependencies };
+          const dependencies = packageJson.dependencies ?? {};
+          const devDependencies = packageJson.devDependencies ?? {};
+          const allDependencies = { ...dependencies, ...devDependencies };
 
-							for (const [packageName, currentVersion] of Object.entries(allDependencies)) {
-									console.log('2222', packageName);
-									const latestVersion = await getLatestVersion(packageName);
-									if (!editor || editor.document.uri.toString() !== document.uri.toString()) {
-											// Editor might have been closed, stop processing further
-											break;
-									}
-									console.log('3333', latestVersion);
-									const regex = new RegExp(`"${packageName}":\\s*"[^"]+"`, 'g');
-									const text = document.getText();
-									let match;
-									while ((match = regex.exec(text)) !== null) {
-											const startPos = document.positionAt(match.index);
-											const endPos = document.positionAt(match.index + match[0].length);
-											const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: `Latest version: ${latestVersion}`, renderOptions: { after: { contentText: `Latest: ${latestVersion}`, color: 'lightgrey' } } };
-											decorationsArray.push(decoration);
-									}
-							}
-
-							if (editor && editor.document.uri.toString() === document.uri.toString()) {
-                editor.setDecorations(decorationType, decorationsArray);
-							}
-					}
-			}
-	}));
+          for (const [packageName, currentVersion] of Object.entries(
+            allDependencies
+          )) {
+            const latestVersion = await getLatestVersion(packageName);
+            const versionPatchType = compareVersions(
+              currentVersion as string,
+              `^${latestVersion}`
+            );
+            const regex = new RegExp(`"${packageName}":\\s*"[^"]+"`, "g");
+            const text = document.getText();
+            let match;
+            while ((match = regex.exec(text)) !== null) {
+              const startPos = document.positionAt(match.index);
+              const endPos = document.positionAt(match.index + match[0].length);
+              const decoration = {
+                range: new vscode.Range(startPos, endPos),
+                hoverMessage: `Update available: ${latestVersion}`,
+                renderOptions: {
+                  after: {
+                    contentText: `Update available: ${latestVersion}`,
+                    color: packageVersionColorMap[versionPatchType],
+                  },
+                },
+              };
+              decorationsArray.push(decoration);
+            }
+          }
+          editor.setDecorations(decorationType, decorationsArray);
+        }
+      }
+    })
+  );
 }
 
 // This method is called when your extension is deactivated
--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,85 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as https from 'https';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+import * as https from "https";
 export function parsePackageJson(uri: vscode.Uri): any {
-  if (fs.existsSync(uri.fsPath)) {
-      const packageJsonContent = fs.readFileSync(uri.fsPath, 'utf8');
-      return JSON.parse(packageJsonContent);
+  console.log("uri", uri);
+  let path = uri.fsPath;
+  if (/\.git/.test(uri.fsPath)) {
+    path = path.replace(/\.git/g, "");
+  }
+  if (fs.existsSync(path)) {
+    const packageJsonContent = fs.readFileSync(path, "utf8");
+    return JSON.parse(packageJsonContent);
   }
   return null;
 }
 
 export function getLatestVersion(packageName: string): Promise<string> {
   return new Promise((resolve, reject) => {
-      https.get(`https://registry.npmjs.org/${packageName}/latest`, (res) => {
-          let data = '';
-          res.on('data', (chunk) => { data += chunk; });
-          res.on('end', () => {
-              resolve(JSON.parse(data).version);
-          });
-      }).on('error', (err) => {
-          reject(err);
+    https
+      .get(`https://registry.npmjs.org/${packageName}/latest`, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          resolve(JSON.parse(data).version);
+        });
+      })
+      .on("error", (err) => {
+        reject(err);
       });
   });
 }
+
+export function checkOpenFileName() {
+  const activeEditor = vscode.window.activeTextEditor;
+  if (activeEditor) {
+    const document = activeEditor.document;
+    const filePath = document.uri.fsPath; // 获取文件的完整路径
+    const fileName = path.basename(filePath); // 从路径中提取文件名
+
+    if (fileName === "package.json") {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+type VersionDifference = "major" | "minor" | "patch" | "none" | "invalid";
+
+export function compareVersions(
+  version1: string,
+  version2: string
+): VersionDifference {
+  const semverRegex = /(\^|~)(\d+)\.(\d+)\.(\d+)$/;
+
+  // 验证版本号格式
+  if (!semverRegex.test(version1) || !semverRegex.test(version2)) {
+    return "invalid";
+  }
+
+  const v1Parts = version1.match(semverRegex)!.slice(2).map(Number);
+  const v2Parts = version2.match(semverRegex)!.slice(2).map(Number);
+  // 比较 major、minor 和 patch 数字
+  for (let i = 0; i < 4; i++) {
+    if (v1Parts[i] !== v2Parts[i]) {
+      return ["major", "minor", "patch"][i] as VersionDifference;
+    }
+  }
+
+  // 版本号完全相同
+  return "none";
+}
+
+export const packageVersionColorMap: Record<VersionDifference, string> = {
+  major: "#d03050",
+  minor: "#f0a020",
+  patch: "#18a058",
+  none: "#2080f0",
+  invalid: "#999",
+};
